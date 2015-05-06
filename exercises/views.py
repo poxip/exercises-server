db35875bbef85a46a5a35bea2f40b4701040d364
Michal Proszek <michal.proszek@gmail.com>
2015-05-06 14:05:45 +0200

    views: Add /question/<id> resource

--- exercises/views.py
+++ exercises/views.py
@@ -17,3 +17,28 @@ def handle_api_error(error):
 
     return response
 
+@app.route('/question/<id>')
+def question(id):
+    """
+    Get question data without solution
+    :param id: Id of a question
+    :return: question data without solution
+    """
+    if not id.isnumeric() or int(id) <= 0:
+        raise InvalidAPIUsage("id must be an integer greater than 0")
+
+    try:
+        cur = g.db.execute(
+            'SELECT title, content FROM questions WHERE id=?', id
+        )
+    except sqlite3.Error as e:
+        raise DatabaseError("Database error", e)
+
+    row = cur.fetchone()
+    if row is None:
+        raise ResourceNotFound("Question not found")
+
+    return jsonify({
+        'title': row[0],
+        'content': row[1]
+    })
