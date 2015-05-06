45801e01ac6cfb98b434840aed3f7c429292c6cd
Michal Proszek <michal.proszek@gmail.com>
2015-05-06 19:23:30 +0200

    views: Add /question/answer/<id>?answer= resource

--- exercises/views.py
+++ exercises/views.py
@@ -54,3 +54,49 @@ def question(id):
         'title': row[0],
         'content': row[1]
     })
+
+
+@app.route('/question/answer/<id>', methods=['GET'])
+def check_answer(id):
+    """Check if the passed answer is correct
+
+    Compare the answer passed through request to
+    correct question's solution stored in database
+
+    :param id: Id of a question
+    """
+    answer = request.args.get('answer', None)
+    if not id.isnumeric() or int(id) <= 0:
+        raise InvalidUsage(
+            "id must be an integer greater than 0",
+            error_code=ErrorCode.WrongId
+        )
+
+    if (answer is None) or (answer is ''):
+        raise InvalidUsage(
+            "answer not set",
+            error_code=ErrorCode.AnswerNotSet
+        )
+
+    try:
+        cur = g.db.execute(
+            'SELECT solution FROM questions WHERE id=?', id
+        )
+    except sqlite3.Error as e:
+        raise DatabaseError(
+            "Database error",
+            db_error=e,
+            error_code=ErrorCode.DatabaseError
+        )
+
+    row = cur.fetchone()
+    if row is None:
+        raise ResourceNotFound(
+            "question not found",
+            error_code=ErrorCode.QuestionNotFound
+        )
+
+    answer_correct = True if answer is row[0] else False
+    return json_success({
+        'answerCorrect': answer_correct
+    })
