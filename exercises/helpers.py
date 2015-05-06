21e535ab00157ad8024ae4d707ad8eb52f759e84
Michal Proszek <michal.proszek@gmail.com>
2015-05-06 20:04:22 +0200

    helpers: Add normalize function

---
+++ exercises/helpers.py
@@ -0,0 +1,10 @@
+"""
+    API server helpers
+"""
+
+def normalize(text):
+    """Normalize specified string
+
+    :param str text: A string to be normalized
+    """
+    return text.lower().strip()
